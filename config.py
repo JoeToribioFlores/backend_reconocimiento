ec5757b13db4f418e617f8c93806f9c5aa31a5c9
JoeToribioFlores <joealexistf@gmail.com>
2025-06-23 15:15:48 -0500

    Leer config desde variables de entorno

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 db_config = {
-    'host': 'mainline.proxy.rlwy.net',
-    'user': 'root',
-    'password': 'YDxCTVxRsregFXrnOgyGTeTQqWYVeNLF',
-    'database': 'railway',
-    'port': 45838
+    'host': os.environ.get('MYSQL_HOST', 'localhost'),
+    'user': os.environ.get('MYSQL_USER', 'root'),
+    'password': os.environ.get('MYSQL_PASSWORD', ''),
+    'database': os.environ.get('MYSQL_DB', ''),
+    'port': int(os.environ.get('MYSQL_PORT', 3306))
 }
